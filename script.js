3b6706fe1a78a770780ce490238a5f43f9e1014e
stealthman1 <timur.marufiy@gmail.com>
2022-08-02 22:19:03 +0300

    local storage config

--- script.js
+++ script.js
@@ -35,9 +35,11 @@ function lightMode() {
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark')
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
@@ -45,3 +47,13 @@ function switchTheme(event) {
 
 // Event Listener
 toogleSwitch.addEventListener('change', switchTheme)
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toogleSwitch.checked = true;
+        darkMode();
+    }
+}
