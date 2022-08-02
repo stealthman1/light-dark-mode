2d1e3f1ab26843cc0e09978c4cefa6410e000711
stealthman1 <timur.marufiy@gmail.com>
2022-08-02 21:45:52 +0300

    toogling theme with JS

---
+++ script.js
@@ -0,0 +1,14 @@
+const toogleSwitch = document.querySelector('input[type="checkbox"]');
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+
+// Event Listener
+toogleSwitch.addEventListener('change', switchTheme)
